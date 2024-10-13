aad83713ecaf6c69431bb85b20687b78d91c88cd
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-13 01:33:22 -0300

    Update script.js

--- Project-Counter/script.js
+++ Project-Counter/script.js
@@ -1,7 +1,7 @@
 var count = 0;
 
-//Usamos a função para somar ou subtrair o numero apresentado na tela, sendo assim, o "count" será somado com mais 1 ou menos 1, 
-//dependendo de qual botão for clicado na tela como é apresentado no Index.html.
+//Usando a função soma ou subtração no numero apresentado em tela, sendo assim, o "count" será somado com mais 1 ou menos 1, 
+//dependendo de qual botão for clicado, como é apresentado no Index.html.
 function changeCount(num){
     count += num;
     document.getElementById("count").innerHTML = count;
